7f5dad3491480b7219df756437ea64aff043ffb4
sunit <sunitsharma1999@gmail.com>
2025-02-15 22:31:54 +0530

    proj2: Task1 Done

--- src/main/java/edu/berkeley/cs186/database/index/LeafNode.java
+++ src/main/java/edu/berkeley/cs186/database/index/LeafNode.java
@@ -372,12 +372,23 @@ class LeafNode extends BPlusNode {
      */
     public static LeafNode fromBytes(BPlusTreeMetadata metadata, BufferManager bufferManager,
                                      LockContext treeContext, long pageNum) {
-        // TODO(proj2): implement
         // Note: LeafNode has two constructors. To implement fromBytes be sure to
         // use the constructor that reuses an existing page instead of fetching a
         // brand new one.
+        Page page = bufferManager.fetchPage(treeContext, pageNum);
+        Buffer buf = page.getBuffer();
+        byte nodeType = buf.get();
+        assert(nodeType == (byte) 1);
+        Optional<Long> rightSibling = Optional.of(buf.getLong());
 
-        return null;
+        List<DataBox> keys = new ArrayList<>();
+        List<RecordId> rids = new ArrayList<>();
+        int n = buf.getInt();
+        for (int i = 0; i < n; ++i) {
+            keys.add(DataBox.fromBytes(buf, metadata.getKeySchema()));
+            rids.add(RecordId.fromBytes(buf));
+        }
+        return new LeafNode(metadata, bufferManager, page, keys, rids, rightSibling, treeContext);
     }
 
     // Builtins ////////////////////////////////////////////////////////////////
